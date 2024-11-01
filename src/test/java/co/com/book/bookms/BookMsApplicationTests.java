32704d491417ce83a408bcdd039cf53e02689e85
ecastrillon1 <edgar.andrey.castrillon.lopez@gmail.com>
2024-11-01 15:32:05 -0500

    Commit inicial

---
+++ src/test/java/co/com/book/bookms/BookMsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.book.bookms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
